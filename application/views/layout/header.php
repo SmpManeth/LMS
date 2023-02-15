e29d6d4574f0ff02bea4340eac89e5a64bd8fc85
Maneth Pathirana <61646614+SmpManeth@users.noreply.github.com>
2023-02-15 19:29:54 +0530

    minor bugs fixed

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -206,31 +206,13 @@
 
 
 
-                            <form id="header_search_form" class="navbar-form navbar-left search-form" role="search" action="<?php echo site_url('admin/admin/search'); ?>" method="POST">
-
-                                <?php echo $this->customlib->getCSRF(); ?>
-
-                                <div class="input-group">
-
-                                    <input type="text" value="<?php echo set_value('search_text1'); ?>" name="search_text1" id="search_text1" class="form-control search-form search-form3" placeholder="<?php echo $this->lang->line('search_by_student_name'); ?>">
-
-                                    <span class="input-group-btn">
-
-                                        <button type="submit" name="search" id="search-btn" onclick="getstudentlist()" style="" class="btn btn-flat topsidesearchbtn"><i class="fa fa-search"></i></button>
-
-                                    </span>
-
-                                </div>
-
-
-
-                            </form>
+                           
 
                         <?php } ?>
 
                         <div class="navbar-custom-menu">
 
-                            <?php if ($this->rbac->hasPrivilege('language_switcher', 'can_view')) {
+                            <?php if (false) {
 
                             ?>
 
